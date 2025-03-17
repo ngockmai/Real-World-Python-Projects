91458bf7010c73686de7463ac45e24665eb94e8f
Ngoc K Mai <maingockhuong89@gmail.com>
2025-03-17 17:57:56 -0500

    flask basics

--- Flask_Bootcamp/oop_ex1/main.py
+++ Flask_Bootcamp/oop_ex1/main.py
@@ -1,5 +1,5 @@
 class Bank_Account():
-    def __init__(self, owner, balance):
+    def __init__(self, owner, balance = 0):
         self.owner = owner
         self.balance = balance
 
@@ -7,13 +7,38 @@ class Bank_Account():
         return f'Owner: {self.owner}, Balance: {self.balance}'
 
     def deposit(self, balance):
-        return self.balance + balance
+        self.balance = self.balance + balance
+        print("Deposit was accepted")
     
-    def withdraw(self, balance):
-        if self.balance - balance >= 0:
-            return self.balance - balance
+    def withdraw(self, wd_balance):
+        if self.balance - wd_balance >= 0:
+            self.balance = self.balance - wd_balance
+            print("Withdrawal successful")
+        else: 
+            print("Funds not available")
+        
 
-ngoc_account = Bank_Account('Ngoc Mai', 10000)
+# Test
+
+# 1. Instantiate the class
+ngoc_account = Bank_Account('Ngoc Mai', 1000)
+
+# 2. Print the object
 print(ngoc_account)
-ngoc_account.withdraw(1000)
-print(ngoc_account)
+
+# 3. Show the account
+ngoc_account.owner
+
+# 4. Show the balance
+ngoc_account.balance
+
+#5. Make a series of deposits and withdrawals
+ngoc_account.deposit(200)
+print(ngoc_account.balance)
+
+ngoc_account.withdraw(700)
+print(ngoc_account.balance)
+
+# 6. Make a withdraw that exceeds the available balance
+ngoc_account.withdraw(800)
+print(ngoc_account.balance)
