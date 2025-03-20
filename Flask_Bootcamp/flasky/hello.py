da01a085284f57573575e33838d9b53345024b3c
Ngoc K Mai <maingockhuong89@gmail.com>
2025-03-19 23:29:55 -0400

    flask-moment

--- Flask_Bootcamp/flasky/hello.py
+++ Flask_Bootcamp/flasky/hello.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 # from flask import request
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
