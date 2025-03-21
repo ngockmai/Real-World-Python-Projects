7f2dbfd52667f9d37884c7e2e85493d9b9740127
Ngoc K Mai <maingockhuong89@gmail.com>
2025-03-20 20:59:17 -0500

    4.0 Web forms

--- Flask_Bootcamp/flasky/hello.py
+++ Flask_Bootcamp/flasky/hello.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from flask_sqlalchemy import SQLALchemy
+import os
+
+
 # from flask import request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+# Config Database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLALchemy(app)
+
+#Form Classes
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session['name']
+        if old_name is not None and old_name != form.name.data:
+            flash('Your name has been changed!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time = datetime.utcnow(), form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name=None):
@@ -23,3 +51,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+#Role and User model definition
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
