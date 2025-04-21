767a6613203140f6e8522d9da8112319d537fd29
Ngoc K Mai <maingockhuong89@gmail.com>
2025-04-21 18:06:48 -0500

    Add sample data and create a basic template

--- AV-Devices-Tool/devices/views.py
+++ AV-Devices-Tool/devices/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from devices.models import Device
+
 
 # Create your views here.
+def index(request):
+    
+    # Query all devices
+    devices = Device.objects.all().select_related('room__building__campus', 'category')
+    
+    context = {
+        'devices': devices
+    }
+    return render(request, 'devices/index.html', context)
