a4c3890dd45af34e7afbf98ef643d3fcd335ed52
Ngoc K Mai <maingockhuong89@gmail.com>
2025-05-13 22:28:51 -0500

    Update urls.py

--- AV-Devices-Tool/backend/devices/urls.py
+++ AV-Devices-Tool/backend/devices/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
-from .views import DeviceListView, DeviceByRoomView, DeviceByLocationView, DeviceDetailView, LocationListView, LocationDetailView, TreeView
+from .views import DeviceListView, DeviceDetailView, ControllerListView, ControllerDetailView, RoomListView, RoomDetailView
 
 api_urlpatterns = [
     # Devices
     path('devices/', DeviceListView.as_view(), name='device_list'),
-    path('devices/room/<int:room_id>/', DeviceByRoomView.as_view(), name='device_by_room'),
-    path('devices/location/<int:location_id>/', DeviceByLocationView.as_view(), name='device_by_location'),
     path('devices/<int:device_id>/', DeviceDetailView.as_view(), name='device_detail'),
+    path('controllers/', ControllerListView.as_view(), name='controller_list'),
+    path('controllers/<int:controller_id>/', ControllerDetailView.as_view(), name='controller_detail'),
+    path('rooms/', RoomListView.as_view(), name='room_list'),
+    path('rooms/<int:room_id>/', RoomDetailView.as_view(), name='room_detail'),
     
-    # Locations
-    path('locations/', LocationListView.as_view(), name='location_list'),
-    path('locations/<int:location_id>/', LocationDetailView.as_view(), name='location_detail'),
+    # path('devices/room/<int:room_id>/', DeviceByRoomView.as_view(), name='device_by_room'),
+    # path('devices/location/<int:location_id>/', DeviceByLocationView.as_view(), name='device_by_location'),
     
-    # Treeview
-    path('locations/treeview/', TreeView.as_view(), name='treeview'),
+    
+    # # Locations
+    # path('locations/', LocationListView.as_view(), name='location_list'),
+    # path('locations/<int:location_id>/', LocationDetailView.as_view(), name='location_detail'),
+    
+    # # Treeview
+    # path('locations/treeview/', TreeView.as_view(), name='treeview'),
 ]
