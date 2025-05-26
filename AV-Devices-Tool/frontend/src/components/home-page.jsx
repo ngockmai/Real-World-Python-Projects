580c82cc2583b23463ee4b3edb30a33d7af3f446
Ngoc Mai <Ngoc.Mai@umanitoba.ca>
2025-05-26 16:07:44 -0500

    Update home-page.jsx

--- AV-Devices-Tool/frontend/src/components/home-page.jsx
+++ AV-Devices-Tool/frontend/src/components/home-page.jsx
@@ -1,17 +1,22 @@
-import {SidebarProvider} from '@/components/ui/sidebar'
-import Header from './Header'
-import AppSidebar from './sidebar'
-import MainContent from './main-content'
+import LocationTree from "./location-tree"
+// import { Separator } from "@/components/ui/separator"
+// import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import Header from "./header"
 export default function Homepage() {
     return (
-        <SidebarProvider>
-            <div className="flex min-h-screen flex-col">
-                <Header />
-                <div className="flex flex-1">
-                    {/* <AppSidebar />
-                    <MainContent /> */}
+        <div className="flex min-h-screen flex-col">
+            <Header />       
+            <div className="flex flex-1 flex-row gap-4 p-4">
+                <LocationTree />
+                <div className="flex-1 rounded-xl bg-muted/50 p-4">                
+                    {/* Data table */}
+                    <p className="text-center text-muted-foreground">Data Table goes here</p>
                 </div>
             </div>
-        </SidebarProvider>
+            <div className="flex-1 rounded-xl bg-muted/50 p-4">
+                <p className="text-center text-muted-foreground">Footer goes here</p>
+            </div>
+        </div>
+        
     )
 }
