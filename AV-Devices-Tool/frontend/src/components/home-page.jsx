a94fb9d4ce14203a46eba7190a3601c7c2febab4
Ngoc Mai <Ngoc.Mai@umanitoba.ca>
2025-05-28 11:36:11 -0500

    Update home-page.jsx

--- AV-Devices-Tool/frontend/src/components/home-page.jsx
+++ AV-Devices-Tool/frontend/src/components/home-page.jsx
@@ -1,16 +1,48 @@
+import React, { useState } from "react"
 import LocationTree from "./location-tree"
-// import { Separator } from "@/components/ui/separator"
-// import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import DeviceTable from "./device-table"
 import Header from "./header"
+
 export default function Homepage() {
+    const [selectedNodeInfo, setSelectedNodeInfo] = useState(null)
+    
+    const handleNodeSelect = (node) => {
+        console.log('Node data:', node.data.data)
+        if(node && node.data && node.data.data){
+            const {type, originalId, name, apiData} = node.data.data
+            console.log('type:', type, 'originalId:', originalId, 'name:', name, 'apiData:', apiData)
+            setSelectedNodeInfo({
+                arboristNodeId: node.id,
+                type: type,
+                originalId: originalId,
+                name: name,
+                apiData: apiData
+            })
+        } else {
+            setSelectedNodeInfo(null)
+        }
+        
+    }
+
     return (
         <div className="flex min-h-screen flex-col">
             <Header />       
             <div className="flex flex-1 flex-row gap-4 p-4">
-                <LocationTree />
-                <div className="flex-1 rounded-xl bg-muted/50 p-4">                
-                    {/* Data table */}
-                    <p className="text-center text-muted-foreground">Data Table goes here</p>
+                <LocationTree 
+                    onNodeSelect={handleNodeSelect} 
+                    selectedNode={selectedNodeInfo ? {id: selectedNodeInfo.originalId} : null}
+                    />
+                <div className="flex-1 rounded-xl bg-muted/50 p-4">
+                    {selectedNodeInfo ? (
+                        <DeviceTable 
+                        locationType={selectedNodeInfo.type}
+                        locationId={selectedNodeInfo.originalId} 
+                    />
+                    ) : (
+                        <p className="text-center text-muted-foreground">
+                            Select an item from the location tree to see details.
+                        </p> 
+                    )}
                 </div>
             </div>
             <div className="flex-1 rounded-xl bg-muted/50 p-4">
